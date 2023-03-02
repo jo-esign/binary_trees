2128384c04b3caf51b96f190d582d6a9035e0c12
Jo <69627271+jo-esign@users.noreply.github.com>
2023-03-02 21:59:20 +0100

    commit

---
+++ 133-heap_extract.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+* heap_extract - extracts the root node of a Max Binary Heap
+* @root: double pointer to the root node of heap
+* Return: value stored in the root node
+*/
+int heap_extract(heap_t **root)
+{
+	if (!root || !*root)
+		return (0);
+	return (1);
+}
